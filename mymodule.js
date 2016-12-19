d03340bb42f35315c01b4bd9db01af37dbe48817
Pako Arroyo <Jose.Castro@chubb.com>
2016-12-19 14:56:55 -0600

    hazla modular

---
+++ mymodule.js
@@ -0,0 +1,5 @@
+// mymodule.js
+
+module.exports  =  function (param){
+	console.log("This is a module!" + param)
+}
--- program.js
+++ program.js
@@ -1,3 +1,15 @@
+var mymodule = require('./mymodule')
+
+
+
+
+
+
+
+
+
+
+
 // console.log(Number(process.argv[2]) + Number(process.argv[3])+ Number(process.argv[4]));  
 // var result = 0
 // for (var i = 2; i < process.argv.length; i++) {
@@ -5,22 +17,22 @@
 // }
 // console.log(result)
 	
-	var path = require('path')
-	var fs = require('fs')
-	fs.readdir(process.argv[2], function (err, data) {
-	  if (err) throw err;
-	  var array = data.toString('utf8').split(',')
-	 // console.log("extension : " +process.argv[3])
-	for (var i = 0; i < array.length; i++) {
-		// console.log(path.extname(array[i]))
-		if(path.extname(array[i]) == "." + process.argv[3])
-		{
-		console.log(array[i])
-		}
-	}
+	// var path = require('path')
+	// var fs = require('fs')
+	// fs.readdir(process.argv[2], function (err, data) {
+	  // if (err) throw err;
+	  // var array = data.toString('utf8').split(',')
+	 // // console.log("extension : " +process.argv[3])
+	// for (var i = 0; i < array.length; i++) {
+		// // console.log(path.extname(array[i]))
+		// if(path.extname(array[i]) == "." + process.argv[3])
+		// {
+		// console.log(array[i])
+		// }
+	// }
 	  
-	   // console.log(data.toString('utf8').split(','));
-	});
+	   // // console.log(data.toString('utf8').split(','));
+	// });
 
 	// fs.readFile(  process.argv[2], function (err, data) {
 	// if (err) throw err;
