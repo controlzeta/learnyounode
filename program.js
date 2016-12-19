96db6f9690d6848c11a2be4ac54be997edb3f8ba
Pako Arroyo <Jose.Castro@chubb.com>
2016-12-19 10:57:31 -0600

    LS Filtrado

--- program.js
+++ program.js
@@ -1,27 +1,41 @@
-//console.log("Hello World")
-
-// console.log(Number(process.argv[2]) + Number(process.argv[3])+ Number(process.argv[4]))
-
-  
+// console.log(Number(process.argv[2]) + Number(process.argv[3])+ Number(process.argv[4]));  
 // var result = 0
-
 // for (var i = 2; i < process.argv.length; i++) {
   // result += Number(process.argv[i])
 // }
-
 // console.log(result)
 	
-	// var fs = require('fs')
-	// buf = new Buffer(256);
-
-	// buf = fs.readFileSync(process.argv[2])
-
-	// console.log(buf.toString().split('\n').length - 1)
-	
-	
-	
+	var path = require('path')
 	var fs = require('fs')
-	fs.readFile(process.argv[2], function (err, data) {
+	fs.readdir(process.argv[2], function (err, data) {
 	  if (err) throw err;
-	  console.log(data.toString('utf8').split('\n').length -1);
+	  var array = data.toString('utf8').split(',')
+	 // console.log("extension : " +process.argv[3])
+	for (var i = 0; i < array.length; i++) {
+		// console.log(path.extname(array[i]))
+		if(path.extname(array[i]) == "." + process.argv[3])
+		{
+		console.log(array[i])
+		}
+	}
+	  
+	   // console.log(data.toString('utf8').split(','));
 	});
+
+	// fs.readFile(  process.argv[2], function (err, data) {
+	// if (err) throw err;
+	// console.log(data.toString('utf8').split('\n').length -1);
+	// });
+	
+	// function callback(error,data){
+		// var fs = require('fs')
+		// buf = new Buffer(256);
+
+		// buf = fs.readFile(process.argv[2])
+
+		// console.log(buf.toString())
+	// }
+	
+	// callback();
+	
+	
